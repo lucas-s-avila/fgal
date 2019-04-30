8ef413a0e2a040526f2d63d622946037909cb336
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-04-30 20:03:17 -0300

    implementei escala de vetor

--- src/operations/operations.cpp
+++ src/operations/operations.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+
 #include "operations/operations.h"
 
 namespace fgal {
@@ -11,6 +12,10 @@ namespace operations {
 
     types::Vector2D vectors_sum(types::Vector2D v1, types::Vector2D v2) {
         return types::Vector2D(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    types::Vector2D vector_scale(types::Vector2D v, int s) {
+        return types::Vector2D(s * v.x, s * v.y);
     }
 }
 }
