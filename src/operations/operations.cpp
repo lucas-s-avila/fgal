14a334456a64f315347e0be18d151ad8185eb324
Lucas <lucas.sores.avila@gmail.com>
2019-04-30 20:03:25 -0300

    implementei a projeção no eixo x

--- src/operations/operations.cpp
+++ src/operations/operations.cpp
@@ -12,5 +12,9 @@ namespace operations {
     types::Vector2D vectors_sum(types::Vector2D v1, types::Vector2D v2) {
         return types::Vector2D(v1.x + v2.x, v1.y + v2.y);
     }
+
+    types::Vector2D projection_X(types::Vector2D v) {
+        return types::Vector2D(v.x,0);
+    }
 }
 }
