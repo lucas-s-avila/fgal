942e4613f207cf1988a3dd7a772ba963c0d19ea6
Thiago <thhiaggo1@hotmail.com>
2019-04-30 20:03:37 -0300

    implementei produto interno

--- src/operations/operations.cpp
+++ src/operations/operations.cpp
@@ -2,15 +2,18 @@
 #include "operations/operations.h"
 
 namespace fgal {
-namespace operations {
-    float points_distance(types::Point2D p1, types::Point2D p2) {
-        return sqrt(
-            pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2)
-        );
-    }
+    namespace operations {
+        float points_distance(types::Point2D p1, types::Point2D p2) {
+            return sqrt(
+                pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2)
+            );
+        }
 
-    types::Vector2D vectors_sum(types::Vector2D v1, types::Vector2D v2) {
-        return types::Vector2D(v1.x + v2.x, v1.y + v2.y);
+        types::Vector2D vectors_sum(types::Vector2D v1, types::Vector2D v2) {
+            return types::Vector2D(v1.x + v2.x, v1.y + v2.y);
+        }
+        types::Vector2D internal_produtic(types::Vector2D v1, types::Vector2D v2){
+            return types::Vector2D(v1.x * v2.x, v1.y + v2.y);
+        }
     }
-}
 }
